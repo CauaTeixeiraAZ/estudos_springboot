3784d7ce6653342d061cbbf6e75005112fd34251
Caua Carvalho <cauateixeirac2@gmail.com>
2025-06-18 01:13:33 -0300

    Finish Order

--- course/src/main/java/com/azriel/course/config/TestConfig.java
+++ course/src/main/java/com/azriel/course/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.azriel.course.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.azriel.course.entities.Order;
 import com.azriel.course.entities.User;
+import com.azriel.course.entities.enums.OrderStatus;
+import com.azriel.course.repositories.OrderRepository;
 import com.azriel.course.repositories.UserRepository;
 
 @Configuration
@@ -17,13 +21,25 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private OrderRepository orderRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 
 		User u1 = new User(null, "Maria Brown", "Maria@gmail.com", "999999999", "12345");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "999999998", "54321");
 		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.DELIVERED, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
+		
+		
 		userRepository.saveAll(Arrays.asList(u1, u2));
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		
+		
 	}
 	
 	
